c6a8b44921fa6343947ff241f72f9abaccdee30a
Idris Lawal <idris.lawal@gloopro.com>
2021-02-21 01:50:06 +0100

    fixed a bug in user registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -56,7 +57,7 @@ class User extends Authenticatable
         return static::create([
             'name' => request()->name,
             'email' => request()->email,
-            'password' => request()->password
+            'password' => Hash::make(request()->password)
         ]);
     }
 
