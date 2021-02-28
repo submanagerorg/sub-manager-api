e54607680e764931e241a367d1ac46de6c2f3039
Idris Lawal <idris.lawal@gloopro.com>
2021-02-28 22:18:41 +0100

    added logout endpoint

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', 'App\Http\Controllers\AuthController@login')->name('login');
 
-Route::post('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
+Route::get('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
 
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 
