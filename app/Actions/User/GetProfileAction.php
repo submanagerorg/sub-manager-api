e9cd029ab1aaeeb439fdd26e13e566a0e625b2a2
Onuora Stacey <31920666+PinkCode15@users.noreply.github.com>
2024-07-20 10:30:43 +0100

    get user profile (#54)
    Co-authored-by: StaceyCreates <stacey.onuora@mypatricia.co>

---
+++ app/Actions/User/GetProfileAction.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Actions\User;
+
+use App\Traits\FormatApiResponse;
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+class GetProfileAction
+{
+    use FormatApiResponse;
+    
+   /**
+    * Get user profile
+    *
+    * @return JsonResponse
+    */
+    public function execute(): JsonResponse
+    {
+        try{
+            
+            $user = auth()->user();
+    
+            return $this->formatApiResponse(200, 'User profile retrieved successfully', ['user' => $user]);
+
+        } catch(Throwable $e) {
+            logger($e);
+            return $this->formatApiResponse(500, 'Error occured', [], $e->getMessage());
+        }
+       
+    }
+}
