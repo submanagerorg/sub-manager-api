739bb1eefdebcf55f074917e857a66c365518a18
Onuora Stacey <31920666+PinkCode15@users.noreply.github.com>
2024-07-20 10:38:20 +0100

    get user profile (#54) (#55)
    Co-authored-by: StaceyCreates <stacey.onuora@mypatricia.co>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Actions\User\EditProfileAction;
+use App\Actions\User\GetProfileAction;
 use App\Http\Requests\User\EditProfileRequest;
 
 class UserController extends Controller
@@ -11,5 +12,10 @@ class UserController extends Controller
     public function editProfile(EditProfileRequest $request)
     {
         return (new EditProfileAction())->execute($request->validated());
+    }
+
+    public function getProfile()
+    {
+        return (new GetProfileAction())->execute();
     }
 }
