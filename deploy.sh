2590f6431fd805c8d8f485221686de6a1ff6a6ff
Onuora Stacey <31920666+PinkCode15@users.noreply.github.com>
2024-07-22 23:51:52 +0100

    update deployment pipeline (#59)
    Co-authored-by: StaceyCreates <stacey.onuora@mypatricia.co>

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,22 @@
 
 echo "Running deployment script"
 
+# Check if the number of arguments provided is correct
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <branch>"
+    exit 1
+fi
+
+# Assign the first argument to branch
+branch=$1
+
+echo "Deploying to branch: ${branch}"
+
 # Navigate to your Laravel project directory
 cd /var/www/subsync
 
-# Pull the latest changes from your GitHub repository
-git pull origin main
+# Pull the latest changes from your GitHub repository for the specified branch
+git pull origin $branch
 
 # Install or update Composer dependencies
 composer install --no-interaction --prefer-dist
@@ -21,3 +32,4 @@ php artisan cache:clear
 sudo systemctl restart nginx
 
 echo "Done ✅"
+echo "Deployed to ${branch} ✅"
