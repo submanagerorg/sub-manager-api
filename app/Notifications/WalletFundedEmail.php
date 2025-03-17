d86aa5841d29fe255f54f114dfc4e61849862f77
PinkCode15 <juliehiva@gmail.com>
2025-03-17 18:58:58 +0100

    fix mail

--- app/Notifications/WalletFundedEmail.php
+++ app/Notifications/WalletFundedEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class WalletFundedEmail extends Notification
+class WalletFundedEmail extends Notification implements ShouldQueue
 {
     use Queueable;
 
