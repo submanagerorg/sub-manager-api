856e029a76eba40cef7b84ff0483a0de1384aa09
Onuora Stacey <31920666+PinkCode15@users.noreply.github.com>
2025-03-17 18:43:02 +0100

    fix reversal mail (#101)
    * update fee
    
    * fix
    
    * fix reversal mail
    
    * fix reversal mail

--- app/Notifications/WalletReversedEmail.php
+++ app/Notifications/WalletReversedEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class WalletReversedEmail extends Notification
+class WalletReversedEmail extends Notification implements ShouldQueue
 {
     /**
      * Create a new notification instance.
